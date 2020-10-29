6e7c981c77304aeae34f93243ceb0d5e23d1384e
NzStack <ign.roa@gmail.com>
2020-10-29 12:37:14 -0300

    django web1, primer commit

---
+++ appcelular/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppcelularConfig(AppConfig):
+    name = 'appcelular'
---
+++ appcelular/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Celular(models.Model):
+    marca = models.TextField(max_length=15)
+    modelo = models.TextField(max_length=30)
+    precio = models.PositiveIntegerField(max_length=8)
+
+    def __str__(self) :
+        return self.marca + " " + self.modelo
